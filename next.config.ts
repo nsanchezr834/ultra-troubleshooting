3f3a572cb839a5799d88c52fc40e3351953e1942
nsanchezr834 <nsanchezr834@gmail.com>
2026-07-02 11:02:35 -0600

    fix(stt): add webpack config for transformers.js to prevent undefined object crash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "sharp$": false,
+        "onnxruntime-node$": false,
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
